46ca643c30b37d3191e299b35428b2cff7eba46f
mohankku <mohan.cbein@gmail.com>
2015-08-28 18:33:07 +0000

    initial code

---
+++ src/preprocessors/spp_stream6.h
@@ -0,0 +1,34 @@
+/* $Id: spp_stream6.h,v 1.2 2015/04/23 18:28:10 jocornet Exp $ */
+
+/*
+ * Copyright (C) 2014-2015 Cisco and/or its affiliates. All rights reserved.
+ * Copyright (C) 2004-2013 Sourcefire, Inc.
+**
+** Author: Steven Sturges
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License Version 2 as
+** published by the Free Software Foundation.  You may not use, modify or
+** distribute this program under any other version of the GNU General
+** Public License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __SPP_STREAM6_H__
+#define __SPP_STREAM6_H__
+
+#include "decode.h"
+#include "stream_common.h"
+
+/* list of function prototypes for this preprocessor */
+void SetupStream6(void);
+
+#endif  /* __SPP_STREAM6_H__ */
